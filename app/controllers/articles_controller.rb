9a775abeb8b6bc81579120fa2990fde684582974
briac louazel <briac@laposte.net>
2021-04-30 21:26:44 +0200

    add eit action in articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,15 +6,30 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Votre article a été crée avec succès !"
+      flash[:notice] = 'Votre article a été crée avec succès !'
       redirect_to article_path(@article)
     else
-      render "new"
+      render 'new'
     end
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "L'article a été modifié"
+      redirect_to article_path(@article)
+    else
+      flash[:notice] = "L'article n'a pas pu été modifié"
+      render 'edit'
+    end
   end
 
   private
